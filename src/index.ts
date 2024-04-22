1368f17d7eeb7c22364a9b9fdb8c9b05126310d6
ajin thomas <ajinthomas619@gmail.com>
2024-04-22 09:56:35 +0530

    changed port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import {app } from './app';
 import connectDB from './config/db';
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 const start = async() =>{
     try{
         console.log('hi da');
